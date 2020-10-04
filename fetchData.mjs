fe3115536e21ef5be6e254b4c9d1356c24b43c8d
cobertos <me+git@cobertos.com>
2020-10-04 06:13:08 -0400

    Added explicit STDIN flag so that CI can work properly, added argparse

--- fetchData.mjs
+++ fetchData.mjs
@@ -4,6 +4,7 @@ import chalk from 'chalk';
 import jsdom from 'jsdom';
 import punycode from 'punycode';
 import mapLimit from 'async/mapLimit.js';
+import argparse from 'argparse';
 const { JSDOM } = jsdom;
 
 // For future reference, here are some sources of information we passed over
@@ -261,13 +262,15 @@ async function read(stream) {
   return Buffer.concat(chunks).toString('utf8');
 }
 
-async function main() {
-  // Check stdin for any previous data
-  const inStr = await read(process.stdin);
-  let prevData;
+// Checks stream for any previously output data to use to supplement request
+// heavy portions of the update loop
+async function readPrevious(stream) {
+  const inStr = await read(stream);
   if(inStr) {
+    // Parse the previous data and return the JSON
+    // object, but with the TLDs mapped to keys
     const inObj = JSON.parse(inStr);
-    prevData = inObj
+    return inObj
       .map(({tld, isBrand, hasRestrictions}) => {
         return {
           [tld]: { isBrand, hasRestrictions }
@@ -275,8 +278,26 @@ async function main() {
       })
       .reduce(Object.assign, {});
   }
+}
+
+async function main() {
+  // Parse args
+  const parser = new argparse.ArgumentParser({
+    description: 'Fetch TLD Data'
+  });
+
+  // TODO: Version... why does node make it so hard to just make normal modules
+  // work...
+  parser.add_argument('-v', '--version', { action: 'version', version: '1.1.0' });
+  parser.add_argument('-s', '--stdin', { action: 'store_true', help: 'Read previously output data on STDIN to reuse some old data to reduce amount of web scraping requests needed.' });
+  parser.add_argument('--color', { action: 'store_true', help: 'Pass in for chalk to force color output (should work by default... but doesnt)' });
+
+  const args = parser.parse_args();
+  let prevData;
+  if(args.stdin) {
+    prevData = await readPrevious(process.stdin);
+  }
   const tldObjs = await getTLDData(prevData);
-  // Output
   process.stdout.write(JSON.stringify(tldObjs, null, 2));
 }
 main();
